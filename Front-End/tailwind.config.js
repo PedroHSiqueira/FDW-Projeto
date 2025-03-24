52fe2ba9b51c4fee1a06e19f147fbd4d947f9faa
uPedroLima11 <pedromendeslima2016@gmail.com>
2025-03-24 18:46:24 -0300

    Navbar and Banner

--- Front-End/tailwind.config.js
+++ Front-End/tailwind.config.js
@@ -1,3 +1,4 @@
+
 import {colors} from "./src/styles/colors"
 import {fontFamily} from "./src/styles/fontFamily"
 
@@ -5,7 +6,7 @@ import {fontFamily} from "./src/styles/fontFamily"
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     // NOTE: Update this to include the paths to all of your component files.
-    content: ["./src/**/*.{ts,tsx}"],
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
     presets: [require("nativewind/preset")],
     theme: {
       extend: {
