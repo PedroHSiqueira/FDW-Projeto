44b7c432e5873047fa82bb55ae4e0d46d30565e3
marcelolemes1340 <134510125+marcelolemes1340@users.noreply.github.com>
2025-05-21 10:19:30 -0300

    Delete src/app/(home)/(Auth)/esqueci.tsx
    Remover Esqueci a senha

--- src/app/(home)/(Auth)/esqueci.tsx
+++
@@ -1,8 +0,0 @@
-import { Text, ScrollView } from "react-native";
-export default function Esqueci() {
-  return (
-    <ScrollView className="mt-0">
-      <Text>alguem esqueceu a senhaa</Text>
-    </ScrollView>
-  );
-}
